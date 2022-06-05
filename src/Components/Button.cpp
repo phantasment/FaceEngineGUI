be62b136b3d2fae35854d41049407fcc7ff577f1
Sparky22296 <awesome22296@gmail.com>
2022-06-05 16:48:58 +1000

    Removed unused include.

--- src/Components/Button.cpp
+++ src/Components/Button.cpp
@@ -1,5 +1,4 @@
 #include "FaceEngineGUI/Components/Button.h"
-#include <iostream>
 
 namespace FaceEngineGUI
 {
@@ -70,7 +69,7 @@ namespace FaceEngineGUI
         {
             return;
         }
-        
+
         renderer->Draw(Texture, Bounds, sourceRectangle);
     }
 }
